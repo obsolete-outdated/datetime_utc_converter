f404fd1b7747abd873e08730ccfd5d69214aaa04
Aleksei <54249358+Moonspeaker@users.noreply.github.com>
2020-02-25 06:07:53 +0300

    fixed backwards conversion

--- test/test.dart
+++ test/test.dart
@@ -3,5 +3,6 @@ import 'package:datetime_utc_converter/datetime_utc_converter.dart';
 void main() {
   final DateTime now = DateTime.now();
   print("now: ${now.millisecondsSinceEpoch}");
-  print("at utc: ${dateTimeConvertedToUTC(DateTime.now()).millisecondsSinceEpoch}");
+  print("at utc: ${dateTimeConvertedToUTC(now).millisecondsSinceEpoch}");
+  print("to utc and back: ${dateTimeConvertedToLocal(dateTimeConvertedToUTC(now)).millisecondsSinceEpoch}");
 }
